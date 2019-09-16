29177d38052782487d4e4359a3eeaef506b29055
Michiel de Jong <michiel@unhosted.org>
2019-09-16 15:22:54 +0200

    IRepresentationMetadata

--- src/ldp/IRepresentation.ts
+++ src/ldp/IRepresentation.ts
@@ -1,7 +1,10 @@
 import * as Stream from 'stream';
+import IRepresentationMetadata from './IRepresentationMetadata';
 
 /**
  * A representation of a resource.
  */
-export default interface IRepresentation extends Stream.Readable {
+export default interface IRepresentation {
+  data: Stream.Readable;
+  metadata: IRepresentationMetadata;
 }
