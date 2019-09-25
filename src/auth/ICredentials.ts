feea1f167456c164c4a5fe7349cd874fee08fc52
Michiel de Jong <michielbdejong@users.noreply.github.com>
2019-09-25 17:16:15 +0200

    Update ICredentials.ts

--- src/auth/ICredentials.ts
+++ src/auth/ICredentials.ts
@@ -1,4 +1,5 @@
 export default interface ICredentials {
-  webId: string;
-  authenticated: boolean;
+  // see https://github.com/inrupt/solid-server-ts/issues/5
+  identity: string; // for instance webid claimed
+  validations: Array<any>; // for instance [ true ] if authenticated, [] if not
 }
