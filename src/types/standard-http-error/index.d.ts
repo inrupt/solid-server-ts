21f952d1960f480c994d84208562796afce1b965
Michiel de Jong <michielbdejong@users.noreply.github.com>
2019-09-25 16:28:37 +0200

    Add comment to fix #7

--- src/types/standard-http-error/index.d.ts
+++ src/types/standard-http-error/index.d.ts
@@ -2,6 +2,8 @@ declare module 'standard-http-error' {
   class HttpError extends Error {
     constructor(status: number, properties?: object);
 
+    // the following list is not intended to be complete,
+    // we'll add to it as needed.
     static BAD_REQUEST: number;
     static UNAUTHORIZED: number;
     static FORBIDDEN: number;
