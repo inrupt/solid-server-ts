9443bc258819dafc9bce9d3d8db4ae2a4f95852d
Michiel de Jong <michiel@unhosted.org>
2019-10-04 12:59:42 +0200

    address https://github.com/inrupt/solid-server-ts/pull/28#discussion_r328186723

--- src/auth/ICredentials.ts
+++ src/auth/ICredentials.ts
@@ -1,5 +1,5 @@
 export default interface ICredentials {
   // see https://github.com/inrupt/solid-server-ts/issues/5
   identity: string; // for instance webid claimed
-  validations: Array<any>; // for instance [ true ] if authenticated, [] if not
+  validations: string[]; // for instance [ 'https://oidc.example.com/' ] if authenticated, [] if not
 }
